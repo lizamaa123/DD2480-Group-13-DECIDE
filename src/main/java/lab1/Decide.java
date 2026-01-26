f4900340a9f9f6a3b0e0ef52b736168b653c62f4
Omar Almassri <omar.a.almassri@gmail.com>
2026-01-27 00:09:05 +0100

    Refactor: Extract triangle area logic to helper method and update LIC3

--- src/main/java/lab1/Decide.java
+++ src/main/java/lab1/Decide.java
@@ -105,14 +105,18 @@ public class Decide {
         return CompType.GT;
     }
 
+    // Shoelace formula for area of triangle
+    private static double calculateTriangleArea(double x1, double y1, double x2, double y2, double x3, double y3) {
+        return 0.5 * Math.abs(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2));
+    }
+
     public static boolean lic3() {
         for(int i = 0; i < NUMPOINTS - 2; i++) {
             double x1 = X[i], x2 = X[i + 1];
             double x3 = X[i + 2], y1 = Y[i];
             double y2 = Y[i + 1], y3 = Y[i + 2];
 
-            // Shoelace formula for area of triangle
-            double area = 0.5 * Math.abs(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2));
+            double area = calculateTriangleArea(x1, y1, x2, y2, x3, y3);
 
             if(area > PARAMETERS.AREA1) {
                 return true;
